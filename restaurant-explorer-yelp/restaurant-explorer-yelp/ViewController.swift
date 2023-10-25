6e53c28ce66f5868f4678490299795d304b63358
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2023-10-25 17:14:54 +0200

    Create project

---
+++ restaurant-explorer-yelp/restaurant-explorer-yelp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  restaurant-explorer-yelp
+//
+//  Created by Miguel Mañas Alvarez on 25/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
