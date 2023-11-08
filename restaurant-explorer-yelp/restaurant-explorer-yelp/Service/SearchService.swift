e2e5ea7ce9bf915badccec7130d7a4dbaeb39f68
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2023-11-08 21:01:44 +0100

    Add NetworkManager, SearchService

---
+++ restaurant-explorer-yelp/restaurant-explorer-yelp/Service/SearchService.swift
@@ -0,0 +1,32 @@
+//
+//  SearchService.swift
+//  restaurant-explorer-yelp
+//
+//  Created by Miguel Mañas Alvarez on 8/11/23.
+//
+
+import Foundation
+
+protocol SearchServiceProtocol {
+    func search(word: String, completion: @escaping (Result<Businesses, Error>) -> Void)
+}
+
+class SearchService: SearchServiceProtocol {
+    
+    let networkService: NetworkManagerProtocol!
+    init(networkService: NetworkManagerProtocol) {
+        self.networkService = networkService
+    }
+    
+    internal func search(word: String, completion: @escaping (Result<Businesses, Error>) -> Void)  {
+        let queryItems: [URLQueryItem] = [
+            URLQueryItem(name: "location", value: word),
+            URLQueryItem(name: "limit", value: "20")
+        ]
+        
+        let httpRequest = HTTPRequest(method: .get, path: "search", queryItems: queryItems)
+        
+        networkService.performRequest(request: httpRequest, completion: completion)
+        
+    }
+}
