515a533a7d79eb90f48b680e0b61cbd2ca77f732
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2023-11-13 18:13:18 +0100

    Add info into DetailViewController

--- restaurant-explorer-yelp/restaurant-explorer-yelp/Views/DetailView/DetailViewController.swift
+++ restaurant-explorer-yelp/restaurant-explorer-yelp/Views/DetailView/DetailViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var urlAddressLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
     let business: Business!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print("business: \(business)")
+        fillBussinessParams()
     }
     
     init(business: Business) {
@@ -24,5 +31,27 @@ class DetailViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func fillBussinessParams() {
+        self.nameLabel.text = business.name
+        self.addressLabel.text = business.location?.displayFullAdress()
+        self.phoneLabel.text = business.displayPhone
+        self.rateLabel.text =  business.rating?.description
+        self.priceLabel.text = business.price
+        guard let imageURL = business.imageURL, let url = URL(string: imageURL) else { return }
+        self.photoImage.downloadImage(from: url)
+    }
+}
+
+extension UIImageView {
+    func downloadImage(from url: URL) {
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self?.image = image
+                }
+            }
+        }.resume()
     }
 }
